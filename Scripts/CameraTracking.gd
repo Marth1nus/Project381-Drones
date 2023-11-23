88d660d372be26828cb4db8f5a3df36528ed43ac
MichaelS <84720449+MichaelSeale@users.noreply.github.com>
2023-11-23 11:35:37 +0000

    Delete Scripts directory

--- Scripts/CameraTracking.gd
+++
@@ -1,6 +0,0 @@
-extends Camera3D
-
-@export var tracking_target : Node3D
-
-func _process(_delta):
-	look_at(tracking_target.global_position)
--- Scripts/DroneMeshAnimate.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@export var propeller_speed = 100.0
-@export var propellers: Array[Node3D]
-	
-func _process(delta):
-	for propeller in propellers:
-		propeller.rotate_y(delta * propeller_speed)
